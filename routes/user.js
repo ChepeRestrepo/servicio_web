bccd57e360803faa55ed059b1372222b5d34dfcd
ChepeRestrepo <joserestrepo2901@gmail.com>
2024-06-08 09:33:48 -0500

    Se crea la ruta para crear usuarios, ya que aun no existia

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 
 // Obtener todos los usuarios
@@ -24,6 +26,49 @@ router.get("/:userId", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// Crear un nuevo usuario
+router.post(
+  "/",
+  [
+    body("name")
+      .isLength({ min: 3 })
+      .withMessage("El nombre debe tener al menos 3 caracteres"),
+    body("email").isEmail().withMessage("Por favor, introduce un email válido"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("La contraseña debe tener al menos 6 caracteres"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Verificar si el usuario ya existe
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists)
+      return res.status(400).json({ message: "Email ya registrado" });
+
+    // Encriptar la contraseña
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Crear un nuevo usuario
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    try {
+      const savedUser = await user.save();
+      res.status(201).json({ userId: savedUser._id });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+);
 
 // Eliminar un usuario por ID
 router.delete("/:userId", async (req, res) => {
